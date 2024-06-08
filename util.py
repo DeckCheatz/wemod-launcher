4c0d6885600224c638a44f6c3fab2e51684f0a46
Marvin1099 <40715852+marvin1099@users.noreply.github.com>
2024-06-08 11:10:57 +0000

    For extenal laucher suport add envs
    To support external launchers i added some envs and use WINEPREFIX / WINE to get external wine paths

--- util.py
+++ util.py
@@ -71,17 +71,47 @@ def show_message(
 # Function to grab the Steam Compat Data Path
 def get_compat() -> str:
     ccompat = load_conf_setting("SteamCompatDataPath")
+    wcompat = load_conf_setting("WinePrefixPath")
+    if not wcompat and os.getenv("WINE_PREFIX_PATH"):
+        os.environ["WINEPREFIX"] = os.getenv("WINE_PREFIX_PATH")
     ecompat = os.getenv("STEAM_COMPAT_DATA_PATH")
+    nogame = False
     if not ecompat:
         try:
-            exit_with_message(
-                "Not running GE-Proton",
-                "Error, GE-Proton was not selected in the compatibility settings, exiting",
-            )
+            __import__("FreeSimpleGUI")
         except Exception as e:
-            ecompat = os.path.join(SCRIPT_PATH, ".cache", "nogame")
-    if ccompat:
+            ecompat = ccompat
+            nogame = True
+            if not ecompat:
+                ecompat = os.path.join(SCRIPT_PATH, ".cache")
+        else:
+            if os.getenv("WINEPREFIX") or wcompat:
+                ecompat = wcompat
+                if not ecompat:
+                    ecompat = os.getenv("WINEPREFIX")
+                nogame = True
+                wine = os.getenv("WINE")
+                tools = os.getenv("STEAM_COMPAT_TOOL_PATHS")
+                if tools and len(tools.strip(os.pathsep)) > 0:
+                    if wine:
+                        os.environ['STEAM_COMPAT_TOOL_PATHS'] = tools.strip(os.pathsep) + ":" + os.path.dirname(wine)
+                else:
+                    if not wine:
+                        exit_with_message(
+                            "Not wine not found",
+                            "Error, wine not found,\nthe WINE environment variable needs to be set if using extenal runners, exiting",
+                        )
+                    os.environ['STEAM_COMPAT_TOOL_PATHS'] = os.path.dirname(wine)
+            else:
+                exit_with_message(
+                    "Not running wine",
+                    "Error, not running with wine,\nto run with wine you could select Proton in the compatibility settings, exiting",
+                )
+
+    if ccompat and not nogame:
         ecompat = os.path.join(ccompat, ecompat.split(os.sep)[-1])
+    if wcompat and nogame:
+        ecompat = wcompat
     os.makedirs(ecompat, exist_ok=True)
     return ecompat
 
@@ -89,6 +119,11 @@ def get_compat() -> str:
 # Grab steam compat path
 BASE_STEAM_COMPAT = get_compat()
 WINEPREFIX = os.path.join(BASE_STEAM_COMPAT, "pfx")
+if os.path.isfile(os.path.join(BASE_STEAM_COMPAT, "version")):
+    try:
+        os.symlink(BASE_STEAM_COMPAT, os.path.join(BASE_STEAM_COMPAT,"pfx"))
+    except Exception as e:
+        pass
 
 
 # Save a value onto a setting of the configfile
