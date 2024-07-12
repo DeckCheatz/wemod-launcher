087c18408df62981cdcd1aff4d9eb9b90f33e921
Dom Rodriguez <shymega@shymega.org.uk>
2024-07-12 19:16:49 +0100

    refactor: Refactor Log and Configuration utils
    Temporary commit. To be reworded. TODO! FIXME!
    
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- wemod_launcher/utils/configuration.py
+++ wemod_launcher/utils/configuration.py
@@ -1,24 +1,23 @@
 from strictyaml import load
-from path import Path
+from pathlib import Path
 from xdg.BaseDirectory import save_config_path
 from .logger import LoggingHandler
 
 
 class Configuration(object):
-    def __init__(self, path: str = save_config_path("wemod_launcher")):
+    def __init__(self, cfg_path: str = save_config_path("wemod_launcher")):
         # Initialize logger for Config.
-        self.__log = LoggingHandler("Configuration").get_logger()
+        self.__log = LoggingHandler(module_name=__name__).get_logger()
         self.__log.debug("Initializing configuration.")
+
         # TODO: Add try/except block for handling errors.
-        path = Path(path)
-        try:
+        path = Path(cfg_path)
+        if not path.exists():
             path.mkdir()
-        except FileExistsError:
-            self.__log.debug("Configuration directory already exists - ignoring!")
-            pass
+
         cfg_file = path / "config.yml"
-        cfg_file.write_lines([""])
-        self.__config = load(Path(cfg_file).text()).data
+        cfg_file.write_text("")
+        self.__config = load(str(path)).data
         self.__log.debug("Configuration initialized.")
 
     def get(self, section, key):
