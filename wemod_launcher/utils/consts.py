41de6f592e6eb3641c3efd1e3f77e6cefbd22f94
Dom Rodriguez <shymega@shymega.org.uk>
2024-07-13 00:02:08 +0100

    feat: Add initial Consts class, with `pathlib` support
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

---
+++ wemod_launcher/utils/consts.py
@@ -0,0 +1,8 @@
+import os
+from .configuration import Configuration
+from pathlib import Path
+
+class Consts:
+    CFG = Configuration()
+    SCRIPT_RUNTIME_DIR: Path = Path(os.path.dirname(os.path.realpath(__file__))).expanduser()
+    STEAM_COMPAT_DATA_DIR: Path = Path(os.getenv("STEAM_COMPAT_DATA_DIR", CFG.get_key(["steam", "compat_data_dir"]))).expanduser()
