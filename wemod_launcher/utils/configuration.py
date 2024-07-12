d998a147799b70fed1512f86413bd8b68591ef2c
Dom Rodriguez <shymega@shymega.org.uk>
2024-07-13 00:01:26 +0100

    refactor: Move from YAML to TOML
    TOML is easier to read, and I hated dealing with YAML. PyYAML bad.
    
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- wemod_launcher/utils/configuration.py
+++ wemod_launcher/utils/configuration.py
@@ -1,4 +1,6 @@
-from strictyaml import load
+import logging
+import typing
+from tomllib import load
 from pathlib import Path
 from xdg.BaseDirectory import save_config_path
 from .logger import LoggingHandler
@@ -10,15 +12,31 @@ class Configuration(object):
         self.__log = LoggingHandler(module_name=__name__).get_logger()
         self.__log.debug("Initializing configuration.")
 
-        # TODO: Add try/except block for handling errors.
         path = Path(cfg_path)
         if not path.exists():
+            logging.debug("Configuration path does not exist, creating.")
             path.mkdir()
 
-        cfg_file = path / "config.yml"
-        cfg_file.write_text("")
-        self.__config = load(str(path)).data
+        cfg_file = path / "config.toml"
+
+        if not cfg_file.exists():
+            # TODO: Add support to write dummy config.
+            cfg_file.write_text("### WIP ###")
+
+        self.__cfg_path = cfg_file
+
+        self.__config = load(open(str(cfg_file), 'rb'))
+        print(self.__config)
         self.__log.debug("Configuration initialized.")
 
-    def get(self, section, key):
-        return self.__config[section][key]
+    def get_key(self, keys: list[str]) -> typing.Any:
+        try:
+            config = self.__config
+            for key in keys:
+                config = config[key]
+
+            return config
+        except KeyError:
+            self.__log.warning("Unable to get configuration entry, returning empty variable")
+            self.__log.warning(f"Key: {keys}, config path: {self.__cfg_path}")
+            raise KeyError("Unable to find corresponding key in configuration.")
