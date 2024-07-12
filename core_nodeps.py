a05666c11344646b96df4a7591a3732760e6320f
Dom Rodriguez <shymega@shymega.org.uk>
2024-07-12 15:26:57 +0100

    refactor: Update references to renamed modules
    This commit updates the names of imports from previously renamed
    modules.
    
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- core_nodeps.py
+++ core_nodeps.py
@@ -36,7 +36,7 @@ def check_dependencies(requirements_file: str) -> bool:
             try:
                 importlib.import_module(package)
             except ImportError:
-                from coreutils import log
+                from core_utils import log
 
                 log(f"Package '{package}' is missing")
                 ret = False
@@ -56,7 +56,7 @@ def load_conf_setting(
 def save_conf_setting(
     setting: str, value: Optional[str] = None, section: str = DEF_SECTION
 ) -> None:
-    from coreutils import log
+    from core_utils import log
 
     if not isinstance(section, str):
         log("Error adding the given section: Not a string")
