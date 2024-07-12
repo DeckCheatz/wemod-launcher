87f7a1d3c9029cbabef1e820bb1f03d3b8574c37
Dom Rodriguez <shymega@shymega.org.uk>
2024-07-13 00:02:27 +0100

    feat; Get CLI to print STEAM_COMPAT_DATA_DIR
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- wemod_launcher/cli.py
+++ wemod_launcher/cli.py
@@ -1,7 +1,14 @@
+
 from .utils.logger import LoggingHandler
+from .utils.configuration import Configuration
+from .utils.consts import Consts
 
 log = LoggingHandler(__name__).get_logger()
+cfg = Configuration()
+consts = Consts()
+
 
 def main():
     log.info("Welcome to WeMod Launcher!")
-    pass
+
+    print(consts.STEAM_COMPAT_DATA_DIR)
