d2da431960b778ca1a72c4547119f57bd3fcdbdc
Dom Rodriguez <shymega@shymega.org.uk>
2024-07-13 01:37:30 +0100

    chore: Run `black` on `utils/`
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- wemod_launcher/utils/consts.py
+++ wemod_launcher/utils/consts.py
@@ -2,7 +2,15 @@ import os
 from .configuration import Configuration
 from pathlib import Path
 
+
 class Consts:
     CFG = Configuration()
-    SCRIPT_RUNTIME_DIR: Path = Path(os.path.dirname(os.path.realpath(__file__))).expanduser()
-    STEAM_COMPAT_DATA_DIR: Path = Path(os.getenv("STEAM_COMPAT_DATA_PATH", CFG.get_key(["steam", "compat_data_dir"]))).expanduser()
+    SCRIPT_RUNTIME_DIR: Path = Path(
+        os.path.dirname(os.path.realpath(__file__))
+    ).expanduser()
+    STEAM_COMPAT_DATA_DIR: Path = Path(
+        os.getenv(
+            "STEAM_COMPAT_DATA_PATH",
+            CFG.get_key(["steam", "compat_data_dir"]),
+        )
+    ).expanduser()
--- wemod_launcher/utils/logger.py
+++ wemod_launcher/utils/logger.py
@@ -12,14 +12,19 @@ class LoggingHandler(object):
         level: Optional[int] = None,
         log_dir: Optional[str] = save_data_path("wemod-launcher"),
     ):
-        if not module_name:
+        if not module_name or module_name.strip() == "":
             print("Module name is required!")
             print("This IS a bug, contact upstream devs.")
-        module_name = "wemod_launcher_{}".format(module_name.replace(".", "_"))
+        module_name = "wemod_launcher_{}".format(
+            module_name.replace(".", "_")
+        )
         try:
-            if environ["WEMOD_LAUNCHER_DEV_MODE"].lower() in ('true', '1', 't') and level is None:
+            if (
+                getenv("WEMOD_LAUNCHER_DEV_MODE", "False").lower()
+                in ("true", "1", "t")
+                and level is None
+            ):
                 level = logging.DEBUG
-                print(level)
             elif "WEMOD_LAUNCHER_LOG_LEVEL" in environ and level is None:
                 env_log_level = environ["WEMOD_LAUNCHER_LOG_LEVEL"]
                 match env_log_level:
@@ -37,20 +42,22 @@ class LoggingHandler(object):
                         level = logging.INFO
             else:
                 level = logging.INFO
-            print("Log level: ", level)
         except KeyError:
             level = logging.INFO
-            pass 
+            pass
 
         log_dir = Path(log_dir)
         if not log_dir.exists():
             log_dir.mkdir()
 
-        logFormatter = logging.Formatter("%(asctime)s [%(threadName)-12.12s] [%(levelname)-5.5s] %(name)s: %(message)s")
+        logFormatter = logging.Formatter(
+            "%(asctime)s [%(threadName)-12.12s] [%(levelname)-5.5s] %(name)s: %(message)s"
+        )
         root_logger = logging.getLogger(module_name)
 
         file_handler = logging.FileHandler(
-                str(log_dir / "wemod-launcher.log"))
+            str(log_dir / "wemod-launcher.log")
+        )
         file_handler.setLevel(level)
         file_handler.setFormatter(logFormatter)
 
@@ -63,8 +70,9 @@ class LoggingHandler(object):
         root_logger.addHandler(console_handler)
 
         self.__logger = root_logger
-        root_logger.debug(f"Logger for module ({module_name}) initialized with log level ({level})")
-        
+        root_logger.debug(
+            f"Logger for module ({module_name}) initialized with log level ({level})"
+        )
 
     def get_logger(self) -> logging.Logger:
         return self.__logger
