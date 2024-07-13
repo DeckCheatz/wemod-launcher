a7acb1c083b70008e608d6532806d1071ab9a5c0
Dom Rodriguez <shymega@shymega.org.uk>
2024-07-13 11:50:45 +0100

    feat: Add more consts
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- wemod_launcher/utils/consts.py
+++ wemod_launcher/utils/consts.py
@@ -8,6 +8,7 @@ class Consts:
     SCRIPT_RUNTIME_DIR: Path = Path(
         os.path.dirname(os.path.realpath(__file__))
     )
+    SCRIPT_PATH: Path = Path(os.path.realpath(__file__))
     STEAM_COMPAT_DATA_DIR: Path = Path(
         os.getenv(
             "STEAM_COMPAT_DATA_PATH",
