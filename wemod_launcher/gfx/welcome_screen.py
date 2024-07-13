3e4531d36531caa04caa0573d7333361cc394ce5
Dom Rodriguez <shymega@shymega.org.uk>
2024-07-13 14:51:52 +0100

    feat: Add caching for WeMod logo download
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- wemod_launcher/gfx/welcome_screen.py
+++ wemod_launcher/gfx/welcome_screen.py
@@ -1,6 +1,8 @@
+from xdg.BaseDirectory import save_cache_path
 import FreeSimpleGUI as sg
 import requests
-
+from pathlib import Path
+from ..utils.logger import LoggingHandler
 
 class WelcomeScreenGfx(object):
     WEMOD_LOGO_URL: str = (
@@ -9,7 +11,20 @@ class WelcomeScreenGfx(object):
 
     def __init__(self):
         ## TODO: Cache this image.
-        self.WEMOD_LOGO_RAW = requests.get(self.WEMOD_LOGO_URL, stream=False)
+
+        self.__logger = LoggingHandler(__name__).get_logger()
+
+        cache_path = Path(save_cache_path("wemod_launcher")) / "assets/wemod_logo.png"
+        if cache_path.exists():
+            self.WEMOD_LOGO_RAW = open(str(cache_path), "rb").read()
+        else:
+            cache_path.parent.mkdir(parents=True, exist_ok=True)
+            self.__logger.debug("Downloading logo to cache, and memory.")
+            logo_raw = requests.get(self.WEMOD_LOGO_URL, stream=False).content
+            open(str(cache_path), "wb").write(logo_raw)
+            self.WEMOD_LOGO_RAW = open(str(cache_path), "rb").read()
+
+        self.__logger.debug("WelcomeScreenGfx initialized")
 
     def run(self):
         sg.theme("systemdefault")
@@ -18,8 +33,8 @@ class WelcomeScreenGfx(object):
             sg.popup_ok_cancel(
                 "Welcome to the WeMod Installer!\nPress ok to start the setup.",
                 title="WeMod Launcher Setup",
-                image=self.WEMOD_LOGO_RAW.content,
-                icon=self.WEMOD_LOGO_RAW.content,
+                image=self.WEMOD_LOGO_RAW,
+                icon=self.WEMOD_LOGO_RAW,
             )
             == "OK"
         )
