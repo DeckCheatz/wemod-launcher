6b08174519aacb175d28c38722fe97479ef316e5
Dom Rodriguez <shymega@shymega.org.uk>
2024-07-13 10:41:45 +0100

    feat: Add `devenv` support, hooks & flake-compat support
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

---
+++ devenv.nix
@@ -0,0 +1,44 @@
+{ pkgs, ... }:
+{
+  packages = [
+    pkgs.poetry # Environment
+    pkgs.watchman # Pyre
+  ];
+
+  languages = {
+    nix.enable = true;
+    python = {
+      enable = true;
+      package = pkgs.python3Full;
+      poetry = {
+        enable = true;
+        activate.enable = true;
+        install = {
+          enable = true;
+          compile = true;
+          quiet = true;
+        };
+      };
+    };
+    shell.enable = true;
+  };
+
+  difftastic.enable = true;
+  devcontainer.enable = true;
+
+  pre-commit.hooks = {
+    shellcheck.enable = true;
+    shfmt.enable = true;
+    # FIXME: Fix Flake8 lints.
+    #    flake8.enable = true;
+    actionlint.enable = true;
+    nixpkgs-fmt.enable = true;
+    black = {
+      enable = true;
+      settings.flags = "-l 78 -t py312";
+    };
+    # FIXME: Fix Markdown lints.
+    #    markdownlint.enable = true;
+    statix.enable = true;
+  };
+}
