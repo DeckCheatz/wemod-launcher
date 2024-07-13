997331e0fccee8b58cd3517a8de8c3ece77e2145
Dom Rodriguez <shymega@shymega.org.uk>
2024-07-13 14:38:48 +0100

    fix(WineUtils): Fix potential breakages in WinNT path test
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- wemod_launcher/pfx/wine_utils.py
+++ wemod_launcher/pfx/wine_utils.py
@@ -4,7 +4,7 @@ from pathlib import Path, PureWindowsPath, PurePosixPath, WindowsPath
 class WineUtils:
     @staticmethod
     def native_path(path: str) -> PureWindowsPath | PurePosixPath:
-        if "C:" in path or "Z:" in path:
+        if path.startswith("C:") or path.startswith("Z:"):
             return PureWindowsPath(path)
 
         return PurePosixPath(path)
