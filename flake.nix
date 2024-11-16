f11bd9c86c34ce90e88f2be13bcc578fd14da79b
Dom Rodriguez <shymega@shymega.org.uk>
2024-11-16 15:23:02 +0000

    refactor: Migrate to PDM from Poetry
    Also tidy up Flake, use `devenv` inside Flake (and update .envrc), as
    well as restructuring project layout
    
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- flake.nix
+++ flake.nix
@@ -3,14 +3,14 @@
 
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-compat = {
       url = "github:edolstra/flake-compat";
       flake = false;
     };
     devenv.url = "github:cachix/devenv";
-    poetry2nix = {
-      url = "github:nix-community/poetry2nix";
+    pyproject-nix = {
+      url = "github:nix-community/pyproject.nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
@@ -20,34 +20,79 @@
     extra-substituters = "https://devenv.cachix.org";
   };
 
-  outputs = { self, nixpkgs, flake-utils, poetry2nix, devenv, ... }:
+  outputs =
+    { self
+    , nixpkgs
+    , flake-utils
+    , devenv
+    , pyproject-nix
+    , ...
+    }@inputs:
     flake-utils.lib.eachDefaultSystem
-      (system:
+      (
+        system:
         let
           pkgs = nixpkgs.legacyPackages.${system};
-          inherit (poetry2nix.lib.mkPoetry2Nix { inherit pkgs; }) mkPoetryApplication;
         in
         {
-          packages = {
-            wemod-launcher = mkPoetryApplication {
-              projectDir = self;
-              python = pkgs.python311Full;
-              preferWheels = true;
-            };
+          packages = with pkgs; {
+            wemod-launcher =
+              with python3Packages;
+              let
+                freesimplegui = buildPythonApplication rec {
+                  pname = "freesimplegui";
+                  version = "5.1.1";
+                  src = fetchPypi {
+                    inherit pname version;
+                    sha256 = "sha256-LwlGx6wiHJl5KRgcvnUm40L/9fwpGibR1yYoel3ZZPs=";
+                  };
+                };
+                project = pyproject-nix.lib.project.loadPyproject { projectRoot = ./.; };
+                python = python3Full;
+                attrs = project.renderers.buildPythonPackage { inherit python; };
+              in
+              buildPythonApplication (
+                lib.recursiveUpdate attrs {
+                  propagatedBuildInputs = [
+                    freesimplegui
+                    pyinstaller
+                    pyxdg
+                    requests
+                    setuptools
+                    tkinter
+                  ];
+                  dependencies = [ freesimplegui ];
+                  doCheck = false;
+                  pythonImportsCheck = [ "wemod_launcher" ];
+                  meta = with lib; {
+                    mainProgram = "wemod-launcher";
+                    maintainers = with maintainers; [ shymega ];
+                  };
+                }
+              );
             default = self.packages.${system}.wemod-launcher;
+            devenv-up = self.devShells.${system}.default.config.procfileScript;
+            devenv-test = self.devShells.${system}.default.config.test;
           };
 
-          devShells.${system} = devenv.lib.mkShell {
-            inputsFrom = [ self.packages.${system}.wemod-launcher ];
+          devShells.default = devenv.lib.mkShell {
+            inherit inputs pkgs;
             modules = [
-              ({ inputs, pkgs, self, ... }: {
-                imports = [ ./devenv.nix ];
-              })
+              (
+                { pkgs
+                , config
+                , inputs
+                , ...
+                }:
+                {
+                  imports = [ ./devenv.nix ];
+                }
+              )
             ];
           };
-        }) // {
-      overlays.default = final: prev: {
-        inherit (self.packages.${final.system}) wemod-launcher;
-      };
+        }
+      )
+    // {
+      overlays.default = final: prev: { inherit (self.packages.${final.system}) wemod-launcher; };
     };
 }
