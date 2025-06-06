9b56ca3c7e92fef8a6be33eac85915d4c5d88edc
Marvin1099 <40715852+marvin1099@users.noreply.github.com>
2025-06-06 20:14:29 +0200

    Tiny changes and workflow test

--- coreutils.py
+++ coreutils.py
@@ -444,7 +444,7 @@ def get_user_input(
 
 def script_manager() -> None:
     script_name = "wemod-launcher"
-    script_version = "1.526"
+    script_version = "1.527"
     last_name = load_conf_setting("ScriptName")
     last_version = load_conf_setting("Version")
 
