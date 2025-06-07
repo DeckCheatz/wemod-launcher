567ab107734ea151422457ecddb83decfb420394
Dom Rodriguez <shymega@shymega.org.uk>
2025-06-07 21:21:19 +0100

    refactor(wine_utils): Use regex for matching paths
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- src/wemod_launcher/pfx/wine_utils.py
+++ src/wemod_launcher/pfx/wine_utils.py
@@ -1,10 +1,12 @@
-from pathlib import Path, PureWindowsPath, PurePosixPath, WindowsPath
+import re
+from pathlib import PureWindowsPath, PurePosixPath
 
 
 class WineUtils:
     @staticmethod
     def native_path(path: str) -> PureWindowsPath | PurePosixPath:
-        if path.startswith("C:") or path.startswith("Z:"):
+        # Match any Windows drive letter pattern like C:\ or D:/ etc.
+        if re.match(r"^[a-zA-Z]:[\\/]", path):
             return PureWindowsPath(path)
 
         return PurePosixPath(path)
